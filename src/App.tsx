3eb1411b799606b536a07a2e73338982b833b0ec
7eduardocf <eduardocfrota901@gmail.com>
2025-05-05 20:24:52 -0400

    Feito o modal de edição de tarefas

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,46 @@ import FormularioDeTarefas from "./assets/components/Formulario-de-tarefas"
 import ListaDeTarefas from "./assets/components/Lista-de-tarefas"
 import { ITask } from "./assets/Interfaces/Tarefa"
 import { useState } from "react"
+import Modal from "./assets/components/Modal/modal"
 
 
 function App() {
 
     const [taskList, setTaskList] = useState<ITask[]>([])
+    const [taskToUpdate, setTaskToUpdate] = useState<ITask | null>(null)
+    const deleteTask = (id:number)=>{
+        setTaskList(
+            taskList.filter(task =>{
+                return task.id !== id
+            })
+        )
+    }
+
+    const hideOrShowModal = (display: boolean) =>{
+        const modal = document.querySelector("#modal")
+        if(display){
+            modal?.classList.remove("hide")
+        }else{
+            modal?.classList.add("hide")
+        }
+    }
+
+    const editTask = (task:ITask)=>{
+        hideOrShowModal(true)
+        setTaskToUpdate(task)
+    }
 
     return (<>
+        <Modal children={<FormularioDeTarefas btnText="Editar tarefa" taskList={taskList} task={taskToUpdate}/>}/>
         <Header/>
         <main className={styles.main}>
             <div>
                 <h2>O que você vai fazer?</h2>
-                <FormularioDeTarefas btnText={"Criar tarefa"} taskList={taskList} setTaskList={setTaskList}/>
+                <FormularioDeTarefas btnText={"Criar tarefa"} taskList={taskList} setTaskList={setTaskList} />
             </div>
             <div>
                 <h2>Suas tarefas:</h2>
-                <ListaDeTarefas tarefa={""}/>
+                <ListaDeTarefas taskList={taskList} handleDelete={deleteTask} handleEdit={editTask}/>
             </div>
         </main>
         <Footer/>
